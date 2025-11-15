9ec7a4fb3a77f2a563723b0f7e343218c3e76877
Vytautasmart <vm6995c@gre.ac.uk>
2025-11-15 20:41:32 +0000

    Testing Backend Functionality

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,25 +1,23 @@
 // node.js version v16.20.2
-const express = require('express')();
-const app = express();   
-const PORT = 8080;
+const express = require("express")();
+const app = express();
 
 // Pasre JSON bodies
 app.use(express.json());
 
-
 // Health check
-app.get('/page', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send({
-    message: 'Backend is running',
-    extrainfo: 'This is the backend server for the Location-Based Authentication System.'
+    message: "Backend is running",
+    extrainfo:
+      "This is the backend server for the Location-Based Authentication System.",
   });
 });
 
-
-app.post('/page', (req, res) => {
-    const data = req.body || {};
-    res.status(201).send({
-        message: 'Data received successfully',
-        received: data
-    });
+app.post("/page", (req, res) => {
+  const data = req.body || {};
+  res.status(201).send({
+    message: "Data received successfully",
+    received: data,
+  });
 });
