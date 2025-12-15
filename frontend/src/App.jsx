f668aab765d12ff888f0cbebd33db63130966eac
Vytautasmart <vm6995c@gre.ac.uk>
2025-12-15 21:17:02 +0000

    feat(frontend): create basic react components and pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      {/* Navbar content will go here */}
+    </nav>
+  );
+}
+
+export default Navbar;
